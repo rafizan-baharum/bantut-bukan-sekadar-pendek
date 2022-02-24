f8eb74279943ccb978bc89676eb712e5615f2923
rafizan-baharum <rafizan.baharum@canang.com.my>
2022-02-25 06:58:56 +0800

    Initial checkin

---
+++ build.sh
@@ -0,0 +1,4 @@
+git pull
+npm run build
+#sudo mkdir /var/www/html/stories/bantut-bukan-sekadar-pendek
+sudo cp -r public/* /var/www/html/stories/bantut-bukan-sekadar-pendek/.
---
+++ src/App.svelte
@@ -0,0 +1,210 @@
+<script lang="ts">
+</script>
+
+<div class="scroll-progress primary-bg"></div>
+<!-- Start Preloader -->
+<!--     <div class="preloader text-center">
+    <div class="circle"></div>
+</div> -->
+<!-- Start Header -->
+<header class="main-header header-style-1 font-heading">
+    <div class="header-top">
+        <div class="container">
+            <div class="row pt-20 pb-20">
+                <div class="col-md-3 col-xs-6">
+                    <a href="https://garasi.bernama.com"><img class="logo" src="logo.png" alt="Garasi@Bernama"></a>
+                </div>
+                <div class="col-md-9 col-xs-6 text-right header-top-right">
+                    <a href="https://garasi.bernama.com/stories/stunting-is-not-just-about-being-short/">EN</a>
+                    <span class="vertical-divider mr-20 ml-20 d-none d-md-inline"></span>
+                    <a href="https://garasi.bernama.com/stories/bantut-bukan-sekadar-pendek/">BM</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Start Main content -->
+<main class="bg-grey pb-30">
+    <div class="entry-header entry-header-style-2 pb-80 pt-80 mb-50 text-white" style="background-image: url(hero.png)">
+        <div class="container entry-header-content">
+            <h1 class="entry-title mb-50 font-weight-900">
+                    Kanak-Kanak Bantut: Mereka Bukan Sekadar Pendek
+            </h1>
+            <div class="entry-meta align-items-center meta-2 font-small color-white">
+                <p class="mb-5 text-white">
+                    Oleh <span class="author-name font-weight-bold text-white">
+                    Sakina Mohamed, Wan Muhammad Aslah Wan Razali & Muhammad Mikhail Mohazar</span>
+                </p>
+                <p class="mb-5 text-white">
+                    Ilustrasi oleh <span class="author-name font-weight-bold text-white">Ummul Syuhaida Othman
+                    </span>
+                </p>
+                <p class="mb-5 text-white">
+                    Infografik oleh <span class="author-name font-weight-bold text-white">Zabariah Iberahim & Muhamad Adib Zainal Abidin</span>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container single-content">
+        <!--end single header-->
+        <!--figure-->
+        <article class="entry-wraper mb-50">
+           <div class="entry-main-content wow fadeIn animated">
+
+<p><i>"Apa masalahnya kalau anak saya pendek sikit? Biasalah tu, anak orang Malaysia. Takkan nak tinggi macam anak Mat Saleh."</i></p>
+<p>Komen yang mungkin tidak asing lagi dan sering juga kita dengar ketinggian anak "ikut keturunan".</p>
+<p>Inilah juga salah tanggapan yang biasa dicanang terutamanya dalam kalangan orang Asia - tidak mengapa kalau anak pendek sedikit, sudah begitu bakanya!</p>
+<p>Namun, satu kajian yang dijalankan selama enam tahun menujukkan sebaliknya. Rupa-rupanya, <b>semua kanak-kanak di dunia mempunyai kemampuan mencapai ketinggian sama dalam lima tahun pertama kehidupan</b>, tidak kira bangsa atau keturunan.</p>
+<p>Ini bererti sehingga usia lima tahun, semua kanak-kanak serata dunia – tanpa mengira latar etnik atau genetik – mampu berdiri sama tinggi.</p>
+<p>Kajian mendapati yang lebih utama ialah nutrisi cukup, persekitaran sihat dan akses kepada perkhidmatan kesihatan.</p>
+<p>Bagaimana pun, harus kita fahami bahawa masalah kanak-kanak bantut ini bukan sekadar kelihatan pendek berbanding rakan.</p>
+<p>Kajian menunjukkan kanak-kanak bantut mengalami masalah kognitif serius hingga menjurus kepada keciciran akademik. Apabila dewasa, mereka didapati lebih sukar mendapat pekerjaan baik dan senang mendapat penyakit.</p>
+<p>Mungkin ada yang membalas: "Jadi, apa masalahnya? Beri sahajalah nutrisi mencukupi sampai mereka jadi tinggi."</p>
+<p>Malangnya, ia tidak semudah itu.</p>
+
+<p>Jika masalah bantut tidak dikesan dan dibetulkan dalam tempoh 1,000 hari pertama kehidupan (sejak dalam kandungan sehingga usia dua tahun), impaknya kekal seumur hidup, demikian menurut Pertubuhan Kesihatan Sedunia (WHO).</p>
+<p>Namun, untuk memahami betapa seriusnya implikasi ke atas negara dan rakyat Malaysia, kita perlu melihat nilai kerugian yang dialami negara akibat masalah bantut ini.</p>
+
+<h2>KITA SEMUA TERKESAN </h2>
+
+<p>Bantut <i>(stunting)</i> ialah apabila kanak-kanak pendek bagi umur mereka.</p>
+<p>Tetapi, mengapa pula ketinggian seorang kanak-kanak menjadi masalah kepada yang lain?</p>
+
+<p>Bantut adalah penanda ukur yang sering digunakan untuk mengesan masalah malnutrisi kronik dalam kalangan kanak-kanak. Selain pendek, malnutrisi kronik boleh menyebabkan kanak-kanak mengalami masalah kesihatan sepanjang hidup.</p>
+<p>Semua masalah ini akhirnya akan memberi kesan kepada sosioekonomi negara, sekali gus memberi impak kepada seluruh rakyat.</p>
+<p>Pelbagai kajian menunjukkan kanak-kanak bantut mempunyai fungsi kognitif lemah dan mengalami kelewatan perkembangan <i>(developmental delay)</i>. Mereka juga mengalami masalah pembelajaran terutama sekali dalam matematik, membaca dan pembangunan kosa kata reseptif, demikian menurut penyelidik Derek Kok dalam laporannya Stunting in Malaysia: <i>Costs, Causes & Courses For Action</i>.</p>
+<p>Ini kerana kanak-kanak bantut gagal mendapat nutrisi secukupnya ketika tempoh kritikal 1,000 hari pertama kehidupan (yang mencakupi tempoh dalam kandungan sehingga usia dua tahun). Perkembangan otak dan tubuh badan amat sensitif terhadap gangguan nutrisi ketika tempoh ini.</p>
+<p>Harus diingat, gangguan nutrisi tidak semestinya disebabkan kegagalan mendapatkan bekalan makanan sihat dan mencukupi.</p>
+<p>Persekitaran kurang ideal seperti ketiadaan akses air bersih dan perkhidmatan kesihatan baik juga boleh menyebabkan ibu dan anak mudah kehilangan zat apabila terkena penyakit atau jangkitan.</p>
+<p>Dapatan kajian juga menunjukkan apabila dewasa, <i>individu bantut dibayar gaji 20 peratus lebih rendah</i> berbanding mereka yang tidak bantut, kata Kok kepada Bernama.</p>
+<p>Menurut Tinjauan Kesihatan dan Morbiditi 2019 (NHMS 2019), 21.8 peratus kanak-kanak di Malaysia bantut.</p>
+<p>Ini bererti satu daripada lima kanak-kanak di Malaysia terbantut.</p>
+<p>Bayangkan satu generasi rakyat dewasa yang mana satu per lima daripadanya mengalami fungsi kognitif lemah sehingga menjejaskan kemampuan mereka mendapat pekerjaan dan pendapatan baik.</p>
+<p>"Inilah mengapa masalah bantut boleh menjejaskan pasaran kerja dan pembangunan ekonomi negara," kata Kok yang juga penganalisis penyelidikan di Jeffrey Cheah Institute on Southeast Asia dan The Jeffrey Sachs Center on Sustainable Development.</p>
+<p>Negara yang mengalami masalah bantut secara purata kerugian 7 peratus pendapatan per kapita, menurut satu kajian World Bank oleh Galasso and Wagstaff.</p>
+<p>Kajian lain menunjukkan individu bantut mempunyai tahap imuniti yang rendah, jadi mereka lebih mudah mendapat jangkitan. Ketika dewasa pula, mereka cenderung menjadi obes, mendapat penyakit darah tinggi, kardiovaskular dan kencing manis.</p>
+<p>Ini sudah tentu akan membebankan lagi sistem kesihatan awam negara.</p>
+<p>Malah, kajian mendapati kesan bantut boleh diwarisi generasi berikutnya. Wanita yang bantut ketika kecil berisiko tinggi melahirkan anak bantut, menyebabkan masalah itu berulang-ulang.</p>
+
+
+<h2>BANTUT DI MALAYSIA</h2>
+
+<p>Setakat 2019, satu daripada setiap lima kanak-kanak di Malaysia terbantut.</p>
+<p>Jumlah ini melibatkan sekitar 560,000 kanak-kanak – lebih dua kali ganda jumlah populasi Perlis dan lebih besar daripada populasi negara Maldives.</p>
+<p>Kadar bantut kanak-kanak seluruh dunia sebenarnya semakin menurun sejak 20 tahun lalu. Namun situasi di Malaysia agak mengecewakan kerana kadarnya semakin meningkat sejak 2010.</p>
+<p><iframe class="flourish-embed-iframe" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation" scrolling="no" src="https://flo.uri.sh/visualisation/8788117/embed" style="width:100%;height:600px;" title="Interactive or visual content"></iframe></p>
+
+<p>Data World Bank 2018 menunjukkan kadar bantut di Malaysia bagi  kanak-kanak bawah empat tahun pada 2016 (20.7 peratus) jauh lebih tinggi daripada Gaza pada 2014 (7.4 peratus).</p>
+<p>Prevalens bantut di Malaysia juga lebih tinggi daripada negara berpendapatan sederhana yang disenaraikan dalam data seperti Ghana (18.8 peratus), Mexico (12.4 peratus), Kazakhstan (8 peratus) dan Macedonia (4.9 peratus).</p>
+
+<p><iframe class="flourish-embed-iframe" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation" scrolling="no" src="https://flo.uri.sh/visualisation/8788309/embed" style="width:100%;height:600px;" title="Interactive or visual content"></iframe></p>
+
+<h2>SEMUANYA SAMA TERKESAN</h2>
+
+<p>Mungkin ada yang berfikiran masalah bantut ini masalah orang miskin.</p>
+<p>Namun, data NHMS 2019 menunjukkan kadar bantut kanak-kanak daripada keluarga dengan pendapatan isi rumah lebih RM10,000 sebulan tetap tinggi (9.1 peratus).</p>
+<p>Kadar ini masih lebih tinggi berbanding kadar penanda aras negara berpendapatan sederhana tinggi iaitu 6.0 peratus. Ini menunjukkan masalah bantut merentasi semua peringkat pendapatan.</p>
+
+<p><iframe class="flourish-embed-iframe" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation" scrolling="no" src="https://flo.uri.sh/visualisation/8742111/embed" style="width:100%;height:600px;" title="Interactive or visual content"></iframe></p>
+
+<p>Kadar bantut juga tinggi dalam kalangan semua kumpulan etnik. Prevalens paling rendah dalam kalangan kaum Cina, tetapi pada kadar 14.1 peratus, ianya tetap tinggi.</p>
+
+<p>Walaupun kadar bantut menurun apabila tahap pendidikan ibu meningkat, kadar bantut kanak-kanak dalam kalangan ibu yang mendapat pendidikan lanjut masih tinggi (18.6 peratus).</p>
+<p>Status pekerjaan si ibu kelihatan mempengaruhi kadar bantut, di mana kadarnya 38.3 peratus dalam kalangan kanak-kanak yang ibunya tiada pekerjaan. Namun, prevalen bantut tetap tinggi walaupun ibu bekerja, sama ada di sektor swasta (19.4 peratus) dan sektor awam (21.7 peratus). </p>
+
+<p><iframe class="flourish-embed-iframe" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation" scrolling="no" src="https://flo.uri.sh/visualisation/8788510/embed" style="width:100%;height:600px;" title="Interactive or visual content"></iframe></p>
+
+<p>Himpunan data jelas menunjukkan masalah bantut berlaku di seluruh Malaysia, merentasi rakyat pelbagai latar belakang dan pada tahap serius.</p>
+<p>Kita harus akui, masalah malnutrisi di Malaysia amat membimbangkan. Jika isu ini gagal ditangani  dengan segera dan berkesan, kita jugalah yang bakal menanggung akibatnya.</p>
+
+
+            </div>
+            <div class="entry-bottom mt-50 mb-30 wow fadeIn animated">
+                <div class="tags">
+                    <span>Tags: </span>
+                    <a href="https://garasi.bernama.com/tags/StuntingInMalaysia" rel="tag">StuntingInMalaysia</a>
+                    <a href="https://garasi.bernama.com/tags/DerekKok" rel="tag">DerekKok</a>
+                    <a href="https://garasi.bernama.com/tags/Malnutrition" rel="tag">Malnutrition</a>
+                    <a href="https://garasi.bernama.com/tags/WHO" rel="tag">WHO</a>
+                    <a href="https://garasi.bernama.com/tags/ChildHunger" rel="tag">ChildHunger</a>
+                    <a href="https://garasi.bernama.com/tags/MaternalHealth" rel="tag">MaternalHealth</a>
+                    <a href="https://garasi.bernama.com/tags/NHMS" rel="tag">NHMS</a>
+                    <a href="https://garasi.bernama.com/tags/Bernama" rel="tag">Bernama</a>
+                </div>
+            </div>
+            <div class="single-social-share clearfix wow fadeIn animated">
+                <ul class="header-social-network d-inline-block list-inline float-md-right mt-md-0 mt-4">
+                    <li class="list-inline-item text-muted"><span>Share this: </span></li>
+                    <li class="list-inline-item"><a class="social-icon fb text-xs-center" target="_blank" href="https://garasi.bernama.com"><i class="elegant-icon social_facebook"></i></a></li>
+                    <li class="list-inline-item"><a class="social-icon tw text-xs-center" target="_blank" href="https://garasi.bernama.com"><i class="elegant-icon social_twitter "></i></a></li>
+                </ul>
+            </div>
+            <!--author box-->
+            <div class="author-bio p-30 mt-50 border-radius-10 bg-white wow fadeIn animated">
+                <div class="author-info">
+                    <div class="author-description"><i>
+                    This story was produced with the financial support of the European Union in the form of a grant from Internews Malaysia. Its contents are the sole responsibility of Internews and do not necessarily reflect the views of the European Union.
+                    </i>
+                    </div>
+                </div>
+            </div>
+            <!--More posts-->
+            <!--comment form-->
+        </article>
+    </div>
+    <!--container-->
+</main>
+<!-- End Main content -->
+<!-- Footer Start-->
+<footer class="pt-50 pb-20 bg-grey">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="sidebar-widget wow fadeInUp animated mb-30">
+                    <div class="widget-header-2 position-relative mb-30">
+                        <h5 class="mt-5 mb-30">Contact Us</h5>
+                    </div>
+                    <div class="textwidget">
+                        <p>
+                        Questions? E-mail us at garasi.bernama@gmail.com
+                        <p><strong class="color-black">Follow Us</strong><br>
+                        <ul class="header-social-network d-inline-block list-inline color-white mb-20">
+                             <li class="list-inline-item"><a class="fb" href="https://www.facebook.com/bernamaofficial" target="_blank" title="Facebook"><i class="elegant-icon social_facebook"></i></a></li>
+                             <li class="list-inline-item"><a class="tw" href="https://twitter.com/bernamadotcom" target="_blank" title="Tweet now"><i class="elegant-icon social_twitter"></i></a></li>
+                       </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6">
+                <div class="sidebar-widget widget_categories wow fadeInUp animated mb-30" data-wow-delay="0.1s">
+                    <div class="widget-header-2 position-relative mb-30">
+                        <h5 class="mt-5 mb-30">Quick link</h5>
+                    </div>
+                    <ul class="font-small">
+                         <li class="cat-item cat-item-2"><a href="/about">About Us</a></li>
+                         <li class="cat-item cat-item-4"><a href="https://bernama.com">Bernama</a></li>
+                         <li class="cat-item cat-item-4"><a href="http://www.mycheck.my/">MyCheck</a></li>
+                   </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="sidebar-widget widget_tagcloud wow fadeInUp animated mb-30" data-wow-delay="0.2s">
+                    <div class="widget-header-2 position-relative mb-30">
+                        <h5 class="mt-5 mb-30">Tagcloud</h5>
+                    </div>
+                    <div class="tagcloud mt-50">
+                        <a class="tag-cloud-link" href="https://garasi.bernama.com/tags/Health">Health</a>
+                        <a class="tag-cloud-link" href="https://garasi.bernama.com/tags/Politics">Politics</a>
+                        <a class="tag-cloud-link" href="https://garasi.bernama.com/tags/Economy">Economy</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copy-right pt-30 mt-20 wow fadeInUp animated">
+            <p class="float-md-left font-small text-muted">© 2022, Garasi@Bernama</p>
+            <p class="float-md-right font-small text-muted"></p>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
+<div class="dark-mark"></div>
